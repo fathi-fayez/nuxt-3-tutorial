e432d708dfa7fdf4ce99b2da4ab279019955644e
fathi-fayez <fathifayez155@gmail.com>
2024-03-16 23:18:34 +0200

    use router and params

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>home</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam vitae
+      impedit quam asperiores cumque assumenda delectus praesentium natus
+      consequatur veniam? Distinctio saepe, repellendus repellat eius
+      voluptatibus aliquam quibusdam ex aliquid!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+/
+<template>
+  <div>
+    <p>product details for {{ id }}</p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatum
+    cupiditate officia odit quia adipisci laboriosam quo dolore illo molestias!
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style></style>
