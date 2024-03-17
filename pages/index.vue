76d408f71c6680da3e09fd60a1b2b9cba2d28a18
fathi-fayez <fathifayez155@gmail.com>
2024-03-17 23:06:15 +0200

    use Nuxt-link in the project

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <nuxt-link to="/">Nuxt App</nuxt-link>
+        <ul>
+          <li><nuxt-link to="/">Home</nuxt-link></li>
+          <li><nuxt-link to="/about">about</nuxt-link></li>
+        </ul>
+      </nav>
+    </header>
     <h2>home</h2>
     <p>
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam vitae
